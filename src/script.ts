5730e92d4c4d0dfd9f14b26f160d24fe3cd6c352
Mohamed <mohamedbentouhami512@gmail.com>
2024-12-19 11:39:08 +0100

    fetch a random word by using public api

--- src/script.ts
+++ src/script.ts
@@ -6,9 +6,24 @@ const modeGame: HTMLSelectElement = document.getElementById(
   "mode"
 ) as HTMLSelectElement;
 
+// API
+const API_URL = "https://random-word-api.vercel.app/api?words=1";
+
+//Games Variables
+let word: string;
+
 // Events
 btn.addEventListener("click", startGame);
 
-function startGame(): void {
-    console.log(modeGame.value)
+async function startGame(): Promise<void> {
+  await getRandomWord();
+}
+
+async function getRandomWord(): Promise<void> {
+  let response: Response = await fetch(API_URL);
+  if (!response.ok) {
+    throw new Error(`Response status : ${response.status}`);
+  }
+  let data: any = await response.json();
+  word = data[0];
 }
