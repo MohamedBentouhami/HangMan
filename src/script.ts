36708a271e4c043d15c9f14852702aae92de3add
Mohamed <mohamedbentouhami512@gmail.com>
2024-12-19 11:49:25 +0100

    add letters buttons feature

--- src/script.ts
+++ src/script.ts
@@ -5,6 +5,10 @@ const btn: HTMLButtonElement = document.getElementById(
 const modeGame: HTMLSelectElement = document.getElementById(
   "mode"
 ) as HTMLSelectElement;
+const game: HTMLDivElement = document.querySelector(".game") as HTMLDivElement;
+const config: HTMLDivElement = document.querySelector(
+  ".config"
+) as HTMLDivElement;
 
 // API
 const API_URL = "https://random-word-api.vercel.app/api?words=1";
@@ -16,7 +20,9 @@ let word: string;
 btn.addEventListener("click", startGame);
 
 async function startGame(): Promise<void> {
+  config.classList.add("hide");
   await getRandomWord();
+  initLetters();
 }
 
 async function getRandomWord(): Promise<void> {
@@ -27,3 +33,22 @@ async function getRandomWord(): Promise<void> {
   let data: any = await response.json();
   word = data[0];
 }
+
+function initLetters() {
+  let lettersDiv: HTMLDivElement = document.createElement("div");
+  lettersDiv.id = "div_letters";
+  let codeAsciiLetterA: number = "A".charCodeAt(0);
+  let codeAsciiLetterZ: number = "Z".charCodeAt(0);
+
+  for (let i: number = codeAsciiLetterA; i <= codeAsciiLetterZ; i++) {
+    let letter = String.fromCharCode(i);
+    let btn = document.createElement("button");
+    btn.textContent = letter;
+    btn.id = letter;
+    btn.addEventListener("click", play);
+    lettersDiv?.append(btn);
+  }
+  game?.append(lettersDiv);
+}
+
+function play(): void {}
