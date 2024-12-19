147b761e6cc6498e25dbe09e7787350648f794a4
Mohamed <mohamedbentouhami512@gmail.com>
2024-12-19 11:25:24 +0100

    add config game

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("test");
+// DOM
+const btn: HTMLButtonElement = document.getElementById(
+  "btn-start"
+) as HTMLButtonElement;
+const modeGame: HTMLSelectElement = document.getElementById(
+  "mode"
+) as HTMLSelectElement;
+
+// Events
+btn.addEventListener("click", startGame);
+
+function startGame(): void {
+    console.log(modeGame.value)
+}
