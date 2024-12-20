aaca4eee5f9afbd76f6a7d1ba119f9bc9c56a856
Mohamed <mohamedbentouhami512@gmail.com>
2024-12-20 14:02:25 +0100

    add doc and update readme close #1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# HangMan
-A simple Hangman word-guessing game built with TypeScript.
+# 🕹️ Hangman Game
+
+A simple Hangman word-guessing game built with **TypeScript** and **HTML/CSS**.
+
+## 📜 Description
+
+This Hangman game challenges players to guess a random word letter by letter before running out of attempts.
+
+### Features
+
+- 🎮 **Two Difficulty Modes**:
+
+  - **Easy**: Starts with one letter revealed.
+  - **Normal**: No letters revealed at the start.
+
+- 🌐 **Random Word Fetching**: Uses the [Random Word API](https://random-word-api.vercel.app).
+
+## 🛠️ Technologies Used
+
+- **TypeScript**
+- **HTML5**
+- **CSS3**
+- **API**
+
+## 🚀 Project Setup and Installation
+
+1. **Clone the repository** (if applicable):
+   ```bash
+   git clone https://github.com/MohamedBentouhami/HangMan.git
+   ```
+2. **Install dependencies:**
+
+    ```bash
+    npm install
+    ```
+
+3. **Compile the TypeScript files:**
+
+    ```bash
+    npx tsc
+    ```
+
+4. **Open index.html in your browser to start playing!**
--- src/script.ts
+++ src/script.ts
@@ -31,6 +31,9 @@ let givenLetters: string[] = [];
 // Events
 btn.addEventListener("click", startGame);
 
+/**
+ * Initializes the game by setting up the game state and fetching a random word.
+ */
 async function startGame(): Promise<void> {
   config.classList.add("hide");
   await getRandomWord();
@@ -41,6 +44,9 @@ async function startGame(): Promise<void> {
   image.classList.remove("hide");
 }
 
+/**
+ * Fetches a random word from the Random Word API.
+ */
 async function getRandomWord(): Promise<void> {
   let response: Response = await fetch(API_URL);
   if (!response.ok) {
@@ -49,6 +55,10 @@ async function getRandomWord(): Promise<void> {
   let data: any = await response.json();
   word = data[0].toUpperCase();
 }
+
+/**
+ * Initalize letters and assign event the the button
+ */
 
 function initLetters() {
   let lettersDiv: HTMLDivElement = document.createElement("div");
@@ -71,8 +81,11 @@ function initLetters() {
   game?.append(lettersDiv);
 }
 
+/**
+ * Initializes the display of the word by creating a series of underscores ("_") for
+ * each letter.
+ */
 function initHideWord() {
-  console.log(word);
   for (let i: number = 0; i < word.length; i++) {
     let p: HTMLParagraphElement = document.createElement("p");
     p.textContent = "_";
@@ -80,6 +93,10 @@ function initHideWord() {
   }
 }
 
+/**
+ * Handles the click event for a letter button in the Hangman game.
+ * @param {HTMLButtonElement} this - The button clicked by the user, representing the guessed
+ */
 function play(this: HTMLButtonElement): void {
   let letter: string = this.id;
   this.removeEventListener("click", play);
@@ -94,11 +111,18 @@ function play(this: HTMLButtonElement): void {
   }
 }
 
+/**
+ * Updates the display of remaining chances and checks if the game is over.
+ */
 function updateChances(): void {
   chancesText!.textContent = `Chances Left : ${chances}`;
   if (chances == 0) gameOver();
 }
 
+/**
+ * Updates the display by revealing the correct letters in the word.
+ * @param letter The correct letter that was guessed by the player.
+ */
 function printGoodLetters(letter: string): void {
   for (let i: number = 0; i < word.length; i++) {
     if (word[i] === letter) {
@@ -108,6 +132,9 @@ function printGoodLetters(letter: string): void {
   }
 }
 
+/**
+ * Handles the game over state when the player loses the game.
+ */
 function gameOver(): void {
   showMessage("you lost");
   removesAllLetters();
@@ -115,20 +142,29 @@ function gameOver(): void {
   showAnswer();
 }
 
+/**
+ * Displays the given message on the screen.
+ * @param msg the message to be displayed on the screen
+ */
 function showMessage(msg: string): void {
   let txt = document.createElement("p");
   txt.textContent = msg;
   game?.append(txt);
 }
 
+/**
+ * Checks if the player has won the game.
+ */
 function checkIfWin() {
-  console.log(countFoundLetter);
   if (countFoundLetter == word.length) {
     showMessage("you win");
     removesAllLetters();
   }
 }
 
+/**
+ * Removes all letters buttons and disables their click events
+ */
 function removesAllLetters(): void {
   const buttons: any = document.getElementById("div_letters");
   for (let btn of buttons.childNodes) {
@@ -137,13 +173,19 @@ function removesAllLetters(): void {
   }
 }
 
+/**
+ * Displays the correct answer by revealing all the letters of the word.
+ */
 function showAnswer(): void {
   for (let i: number = 0; i < word.length; i++) {
     divWord!.childNodes[i].textContent = word[i];
   }
 }
 
-function setLevel() {
+/**
+ * Sets the level of difficulty for the game.
+ */
+function setLevel(): void {
   let mode: string = modeGame.value;
   if (mode === "Easy") {
     let nb: number = Math.floor(Math.random() * word.length);
